820046876b9f49db90fecae0879b499320d39c46
sirdan <danielsdfullerton@gmail.com>
2021-06-19 17:13:02 +0100

    editable user information, still needs hero edits

--- otb-frontend/src/components/auth/Profile.js
+++ otb-frontend/src/components/auth/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { getSingleUser } from '../../lib/api'
+import { editUser, getSingleUser } from '../../lib/api'
 import useForm from '../../hooks/useForm'
 import { useParams } from 'react-router-dom'
+import { isOwner } from '../../lib/auth'
 
 import {
   Box,
@@ -9,7 +10,21 @@ import {
   Text,
   Heading,
   Image,
-  HStack
+  HStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  FormHelperText,
+  Select
 } from '@chakra-ui/react'
 
 import {
@@ -20,9 +35,9 @@ import {
 
 function Profile() {
   const { id } = useParams()
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { formData, setFormData, formErrors, setFormErrors, handleChange } = useForm({
     username: '',
-    email: '',
     battletag: '',
     sr: '',
     mainrole: '',
@@ -49,10 +64,125 @@ function Profile() {
     getData()
   }, [id, setFormData])
 
-  console.log(formData)
+  const handleCancel = async () => {
+    try {
+      const response = await getSingleUser(id)
+      setFormData(response.data)
+      onClose()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleSave = async (e) => {
+    e.preventDefault()
+    try {
+      await editUser(id, formData)
+      onClose()
+    } catch (err) {
+      setFormErrors(err.response.data)
+    }
+  }
+
   return (
     <Box m={{ base: 4, md: 10 }}>
-      <Heading>Profile</Heading>
+      <HStack>
+        <Heading>Profile</Heading>
+        {
+          isOwner(formData.id) ?
+            <EditIcon onClick={onOpen} boxSize={6} _hover={{ color: 'teal' }} />
+            : ''
+        }
+      </HStack>
+      <Modal
+        isOpen={isOpen}
+        onClose={handleCancel}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Edit your Details:</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl pb={5} id='username'>
+              <FormLabel>Username</FormLabel>
+              <Input
+                type='text'
+                placeholder='Username'
+                onChange={handleChange}
+                value={formData.username}
+                name='username'
+              />
+              {
+                formErrors.username ?
+                  <FormHelperText color='red'>{formErrors.username[0]}</FormHelperText>
+                  :
+                  ''
+              }
+            </FormControl>
+
+            <FormControl pb={5} id='battleTag'>
+              <FormLabel>Overwatch Battle Tag</FormLabel>
+              <Input
+                type='text'
+                placeholder='example#12345'
+                onChange={handleChange}
+                value={formData.battletag}
+                name='battletag'
+              />
+              {
+                formErrors.battletag ?
+                  <FormHelperText color='red'>{formErrors.battletag[0]}</FormHelperText>
+                  :
+                  ''
+              }
+            </FormControl>
+
+            <FormControl pb={5} id='sr'>
+              <FormLabel>Rank</FormLabel>
+              <Input
+                type='number'
+                placeholder='0-5000'
+                onChange={handleChange}
+                value={formData.sr}
+                name='sr'
+              />
+              {
+                formErrors.battletag ?
+                  <FormHelperText color='red'>{formErrors.sr[0]}</FormHelperText>
+                  :
+                  ''
+              }
+            </FormControl>
+
+            <FormControl pb={5} id='sr'>
+              <FormLabel>Main Role</FormLabel>
+              <Select
+                value={formData.mainrole}
+                onChange={handleChange}
+                name='mainrole'
+              >
+                <option value="DPS">DPS</option>
+                <option value="TANK">TANK</option>
+                <option value="SUPPORT">SUPPORT</option>
+              </Select>
+              {
+                formErrors.battletag ?
+                  <FormHelperText color='red'>{formErrors.sr[0]}</FormHelperText>
+                  :
+                  ''
+              }
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleSave}>
+              Save
+            </Button>
+            <Button onClick={handleCancel}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
       <Flex direction={{ base: 'column', lg: 'row' }}>
         <Box m={{ base: 0, md: 2 }} flex='1'>
           <Box mt={10} mb={10}>
@@ -60,19 +190,25 @@ function Profile() {
               <Text fontSize='2xl'>
                 Username: {formData.username ? formData.username : 'None'}
               </Text>
-              <EditIcon _hover={{ color: 'teal' }} />
+
             </HStack>
             <HStack>
               <Text fontSize='2xl'>
                 Battletag: {formData.battletag ? formData.battletag : 'None'}
               </Text>
-              <EditIcon _hover={{ color: 'teal' }} />
+
             </HStack>
             <HStack>
               <Text fontSize='2xl'>
                 Rank: {formData.sr ? formData.sr : 'None'}
               </Text>
-              <EditIcon _hover={{ color: 'teal' }} />
+
+            </HStack>
+            <HStack>
+              <Text fontSize='2xl'>
+                Main Role: {formData.mainrole ? formData.mainrole : 'None'}
+              </Text>
+
             </HStack>
           </Box>
           <Flex m={0} direction='column'>
@@ -112,7 +248,7 @@ function Profile() {
           </Flex>
         </Box>
 
-        <Box flex={1} m={2}>
+        <Box flex={1} m={0}>
           <Text fontSize='3xl'>Teams:</Text>
           <Box border='2px' height='4em' borderColor='gray.600' rounded='5px'>
           </Box>
@@ -123,15 +259,14 @@ function Profile() {
 }
 
 function BannerHeroImg({ heroformData }) {
-  console.log(heroformData)
   return (<Image
     w='32%'
     m={{ base: 0.5, md: 1 }} border='1px'
     rounded='5px'
     borderColor='whiteAlpha.300'
-    src={heroformData ? heroformData.imgBanner : ''}
+    src={heroformData ? heroformData.imgBanner : 'https://res.cloudinary.com/sirdancloud/image/upload/v1624032403/OTB/missing-img-banner_etypbg.png'}
     _hover={{
-      borderColor: 'gray.600',
+      borderColor: 'gray.400',
     }}
   />
   )
--- otb-frontend/src/lib/api.js
+++ otb-frontend/src/lib/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
-// import { getToken } from './auth'
+import { getToken } from './auth'
 
 const URL = '/api'
 
-// function headers() {
-//   return {
-//     headers: { Authorization: `Bearer ${getToken()}` },
-//   }
-// }
+function headers() {
+  return {
+    headers: { Authorization: `Bearer ${getToken()}` },
+  }
+}
 
 export function loginUser(formData) {
   return axios.post(`${URL}/auth/login/`, formData)
@@ -19,4 +19,20 @@ export function registerUser(formdata) {
 
 export function getSingleUser(profileId) {
   return axios.get(`${URL}/auth/profile/${profileId}`)
+}
+
+export function editUser(profileId, formdata) {
+  const formattedFormData = {
+    ...formdata,
+    dps1: formdata.dps1.id,
+    dps2: formdata.dps2.id,
+    dps3: formdata.dps3.id,
+    tank1: formdata.tank1.id,
+    tank2: formdata.tank2.id,
+    tank3: formdata.tank3.id,
+    support1: formdata.support1.id,
+    support2: formdata.support2.id,
+    support3: formdata.support3.id,
+  }
+  return axios.put(`${URL}/auth/profile/${profileId}/`, formattedFormData, headers())
 }
